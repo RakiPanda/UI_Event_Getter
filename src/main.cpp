1bad49f2ca1084911cf9ee8b34e4e66d3a1dd849
RakiPanda <scrumhalf0415@gmail.com>
2025-01-25 23:16:56 +0900

    MouseLoggerを使用してマウスの動きを記録する機能をmain.cppに追加

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,27 @@
-#include "helloworld.h"
+#include <iostream>
+#include <thread>
+#include "MouseLogger.h"
 
 int main() {
-    printHelloWorld();
+    // MouseLoggerインスタンスを作成
+    MouseLogger logger;
+
+    // フックを実行するスレッドを開始
+    std::thread hookThread([&logger]() {
+        logger.Start();
+    });
+
+    // ユーザーが終了するまでプログラムを実行
+    std::cout << "Press Enter to stop logging..." << std::endl;
+    std::cin.get();
+
+    // フックを停止して終了
+    logger.Stop();
+
+    // スレッドを切り離し
+    if (hookThread.joinable()) {
+        hookThread.detach();
+    }
+
     return 0;
 }
