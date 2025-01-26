d41b6965792ece1ebbc5f9e418d4c73e0a1714a1
RakiPanda <scrumhalf0415@gmail.com>
2025-01-26 16:15:01 +0900

    管理者権限での実行を確認する機能を追加

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,28 @@
 #include <iostream>
 #include <thread>
+#include <windows.h>
 #include "UnifiedLoggers.h"
 
+bool IsRunningAsAdmin() {
+    BOOL isAdmin = FALSE;
+    PSID adminGroup = NULL;
+    SID_IDENTIFIER_AUTHORITY ntAuthority = SECURITY_NT_AUTHORITY;
+
+    if (AllocateAndInitializeSid(&ntAuthority, 2, SECURITY_BUILTIN_DOMAIN_RID, DOMAIN_ALIAS_RID_ADMINS, 
+                                 0, 0, 0, 0, 0, 0, &adminGroup)) {
+        CheckTokenMembership(NULL, adminGroup, &isAdmin);
+        FreeSid(adminGroup);
+    }
+
+    return isAdmin;
+}
+
 int main() {
+    if (!IsRunningAsAdmin()) {
+        std::cerr << "This program must be run as an administrator." << std::endl;
+        return 1;
+    }
+
     // UnifiedLoggersインスタンスを作成
     UnifiedLoggers logger;
 
