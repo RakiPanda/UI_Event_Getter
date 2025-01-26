f7091883f3b9f2e5e4f59e593438d75653285c6c
RakiPanda <scrumhalf0415@gmail.com>
2025-01-26 15:05:26 +0900

    KeyboardLogger.hに追加のキーコードとキー名のマッピングを追加

--- src/KeyboardLogger.cpp
+++ src/KeyboardLogger.cpp
@@ -16,7 +16,17 @@ std::map<int, std::string> keyMap = {
     {0x50, "P"}, {0x51, "Q"}, {0x52, "R"}, {0x53, "S"}, {0x54, "T"},
     {0x55, "U"}, {0x56, "V"}, {0x57, "W"}, {0x58, "X"}, {0x59, "Y"},
     {0x5A, "Z"}, {VK_SPACE, "Space"}, {VK_RETURN, "Enter"}, {VK_BACK, "Backspace"},
-    {VK_TAB, "Tab"}, {VK_SHIFT, "Shift"}, {VK_CONTROL, "Ctrl"}, {VK_MENU, "Alt"}
+    {VK_TAB, "Tab"}, {VK_SHIFT, "Shift"}, {VK_CONTROL, "Ctrl"}, {VK_MENU, "Alt"},
+    {VK_CAPITAL, "Caps Lock"}, {VK_ESCAPE, "Esc"}, {VK_PRIOR, "Page Up"}, {VK_NEXT, "Page Down"},
+    {VK_END, "End"}, {VK_HOME, "Home"}, {VK_LEFT, "Left Arrow"}, {VK_UP, "Up Arrow"},
+    {VK_RIGHT, "Right Arrow"}, {VK_DOWN, "Down Arrow"}, {VK_INSERT, "Insert"}, {VK_DELETE, "Delete"},
+    {VK_NUMPAD0, "Numpad 0"}, {VK_NUMPAD1, "Numpad 1"}, {VK_NUMPAD2, "Numpad 2"}, {VK_NUMPAD3, "Numpad 3"},
+    {VK_NUMPAD4, "Numpad 4"}, {VK_NUMPAD5, "Numpad 5"}, {VK_NUMPAD6, "Numpad 6"}, {VK_NUMPAD7, "Numpad 7"},
+    {VK_NUMPAD8, "Numpad 8"}, {VK_NUMPAD9, "Numpad 9"}, {VK_MULTIPLY, "Numpad *"}, {VK_ADD, "Numpad +"},
+    {VK_SUBTRACT, "Numpad -"}, {VK_DECIMAL, "Numpad ."}, {VK_DIVIDE, "Numpad /"}, {VK_F1, "F1"},
+    {VK_F2, "F2"}, {VK_F3, "F3"}, {VK_F4, "F4"}, {VK_F5, "F5"}, {VK_F6, "F6"},
+    {VK_F7, "F7"}, {VK_F8, "F8"}, {VK_F9, "F9"}, {VK_F10, "F10"}, {VK_F11, "F11"},
+    {VK_F12, "F12"}, {VK_NUMLOCK, "Num Lock"}, {VK_SCROLL, "Scroll Lock"}
     // 必要に応じて他のキーも追加
 };
 
