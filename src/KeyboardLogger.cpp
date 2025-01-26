e51af107496278e44dde71b64ea5a1a7bc5f81ad
RakiPanda <scrumhalf0415@gmail.com>
2025-01-26 15:01:20 +0900

    KeyboardLoggerにキーコードと対応するキー名をマッピングする機能を追加

--- src/KeyboardLogger.cpp
+++ src/KeyboardLogger.cpp
@@ -3,10 +3,22 @@
 #include <filesystem>
 #include <iomanip>
 #include <sstream>
+#include <map>
 
 HHOOK KeyboardLogger::hKeyboardHook = NULL;
 std::ofstream KeyboardLogger::logFile;
 std::chrono::time_point<std::chrono::steady_clock> KeyboardLogger::startTime;
+
+std::map<int, std::string> keyMap = {
+    {0x41, "A"}, {0x42, "B"}, {0x43, "C"}, {0x44, "D"}, {0x45, "E"},
+    {0x46, "F"}, {0x47, "G"}, {0x48, "H"}, {0x49, "I"}, {0x4A, "J"},
+    {0x4B, "K"}, {0x4C, "L"}, {0x4D, "M"}, {0x4E, "N"}, {0x4F, "O"},
+    {0x50, "P"}, {0x51, "Q"}, {0x52, "R"}, {0x53, "S"}, {0x54, "T"},
+    {0x55, "U"}, {0x56, "V"}, {0x57, "W"}, {0x58, "X"}, {0x59, "Y"},
+    {0x5A, "Z"}, {VK_SPACE, "Space"}, {VK_RETURN, "Enter"}, {VK_BACK, "Backspace"},
+    {VK_TAB, "Tab"}, {VK_SHIFT, "Shift"}, {VK_CONTROL, "Ctrl"}, {VK_MENU, "Alt"}
+    // 必要に応じて他のキーも追加
+};
 
 KeyboardLogger::KeyboardLogger() {
     std::filesystem::create_directories("./UI_logs");
@@ -33,7 +45,13 @@ LRESULT CALLBACK KeyboardLogger::KeyboardProc(int nCode, WPARAM wParam, LPARAM l
         auto elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(now - startTime).count();
 
         std::stringstream logStream;
-        logStream << "Key Pressed: " << pKeyboard->vkCode << ", Elapsed Time: " << elapsed << " ms";
+        logStream << "Key Pressed: " << pKeyboard->vkCode;
+
+        if (keyMap.find(pKeyboard->vkCode) != keyMap.end()) {
+            logStream << " (" << keyMap[pKeyboard->vkCode] << ")";
+        }
+
+        logStream << ", Elapsed Time: " << elapsed << " ms";
 
         std::cout << logStream.str() << std::endl;
 
